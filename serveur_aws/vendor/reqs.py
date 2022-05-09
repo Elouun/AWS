92f171413af6bbfc8fb089a06a0c9f24e2ccb6f9
Elouun <elouan.baillieu@gmail.com>
2022-05-09 20:52:59 +0200

    09-05v2

--- serveur_aws/vendor/reqs.py
+++ serveur_aws/vendor/reqs.py
@@ -2,4 +2,4 @@ restaurant = 'Select * from Business where name = "?n"'
 
 user = 'select * from Users where name = "?n"'
 
-review_by_user = 'SELECT * FROM Users u,Reviews r where u.user_id = r.user_id and name= "?n"'
+review_by_user = 'SELECT text,stars FROM Users u,Reviews r where u.user_id = r.user_id and name="?n" limit 5'
