05d81baecef93ffff2cddf3b352dfbe6f38576bc
HugoD98 <56720344+HugoD98@users.noreply.github.com>
2022-05-26 14:37:57 +0200

    Update app.py

--- serveur_aws/app.py
+++ serveur_aws/app.py
@@ -48,10 +48,10 @@ def get_categories(list):
 	res = bdd.request(reqfinal, conn)
 	
 	d = {}
-	
+	count = 0
 	for row in res:
-	    d[row[0]] = {"name":row[1],"address":row[2],"city":row[3], "state":row[4],"postal_code":row[5],"latitude":row[6],"longitude":row[7],"stars":row[8]}
-	
+	    d[count] = {"business_id":row[0],"name":row[1],"address":row[2],"city":row[3], "state":row[4],"postal_code":row[5],"latitude":row[6],"longitude":row[7],"stars":row[8]}
+	    count += 1
 	return  json.dumps(d)
 
 
