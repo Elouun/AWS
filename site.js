228abdfe9179ed9eeac4af9fd9b24b94440a8bb3
antoine <adelacoux@infotrafic.com>
2022-06-08 15:51:43 +0200

    image

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require('fs');
-const fsp = require('fs').promise;
+const fsp = require('fs').promises;
 
 const host = '192.168.1.60';
 //const host = 'localhost';
