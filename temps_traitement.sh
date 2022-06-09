892719575f98223b244673f6b71951bcb4348726
antoine <adelacoux@infotrafic.com>
2022-06-09 15:15:47 +0200

    message type

--- temps_traitement.sh
+++ temps_traitement.sh
@@ -1,8 +1,8 @@
 echo "{"
-echo "user:{"
+echo "user:{\""
 time curl https://myxzcnelvk.execute-api.eu-west-3.amazonaws.com/api/getUserid/-_2h2cJlBOWAYrfplMU-Cg
-echo "},restaurant:{"
+echo "\"},restaurant:{\""
 time curl "https://myxzcnelvk.execute-api.eu-west-3.amazonaws.com/api/getCloserRestaurant?lat=39.93450004198443&long=-75.19926226811313"
-echo "},model:{"
+echo "\"},model:{\""
 time python3 ./model/testModel.py 1,2,3,4,5,6,7,8,9,10_1,1,1,1,1,1,1,1,1,1 2> err.log
-echo "}}"
+echo "\"}}"
