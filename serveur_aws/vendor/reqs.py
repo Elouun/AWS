6af932684dfb5e4deb518224dc4bf4e1638bfb1c
antoine <adelacoux@infotrafic.com>
2022-06-09 12:29:10 +0200

    stats site suite

--- serveur_aws/vendor/reqs.py
+++ serveur_aws/vendor/reqs.py
@@ -6,7 +6,7 @@ countRestaurantNameAlmost = 'select count(*) from Business where name like "%?ke
 restaurantID = 'Select * from Business where business_id = "?n"'
 
 user = 'select * from Users where name = "?n"'
-userID = 'Selct * from Users where user_id = "?n"'
+userID = 'Select * from Users where user_id = "?n"'
 
 review_by_user = 'SELECT text,stars FROM Users u,Reviews r where u.user_id = r.user_id and name="?n" limit 5'
 review_by_user_id = 'SELECT text,stars FROM Users u,Reviews r where u.user_id = r.user_id and user_id="?n" limit 5'
