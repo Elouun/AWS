66543726c177d6de6b5469b45e0718b3a0716be4
antoine <adelacoux@infotrafic.com>
2022-06-09 15:23:37 +0200

    message type

--- temps_traitement.sh
+++ temps_traitement.sh
@@ -1,6 +1,6 @@
 echo "user:"
-time curl https://myxzcnelvk.execute-api.eu-west-3.amazonaws.com/api/getUserid/-_2h2cJlBOWAYrfplMU-Cg
+time curl https://myxzcnelvk.execute-api.eu-west-3.amazonaws.com/api/getUserid/-_2h2cJlBOWAYrfplMU-Cg > /dev/null
 echo "restaurant:"
-time curl "https://myxzcnelvk.execute-api.eu-west-3.amazonaws.com/api/getCloserRestaurant?lat=39.93450004198443&long=-75.19926226811313"
-echo "model:'"
-time python3 ./model/testModel.py 1,2,3,4,5,6,7,8,9,10_1,1,1,1,1,1,1,1,1,1 2> err.log
+time curl "https://myxzcnelvk.execute-api.eu-west-3.amazonaws.com/api/getCloserRestaurant?lat=39.93450004198443&long=-75.19926226811313 > /dev/null"
+echo "model:"
+time python3 ./model/testModel.py 1,2,3,4,5,6,7,8,9,10_1,1,1,1,1,1,1,1,1,1 2> err.log > /dev/null
