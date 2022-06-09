4c93f2c91a20080bf7d490a6a4b4bb0c9f0deb01
antoine <adelacoux@infotrafic.com>
2022-06-09 15:11:55 +0200

    message type

--- site.js
+++ site.js
@@ -121,6 +121,8 @@ const requestListener = async function (req, res) {
 				const cmd_tps = "bash /home/pi/AWS/temps_traitement.sh"
 				let result_tps_cmd = await commande_aws(cmd_tps);
 				//result_tps_cmd = JSON.parse(result_tps_cmd);
+				console.log("Next")
+
 				console.log(result_tps_cmd)
 
 
