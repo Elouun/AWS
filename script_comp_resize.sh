baaade8029578aaac4b6882e449669c4445b19e4
DeAntoine <47144615+DeAntoine@users.noreply.github.com>
2022-06-13 11:29:02 +0200

    Update script_comp_resize.sh

--- script_comp_resize.sh
+++ script_comp_resize.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 mkdir processed
-i=10116
+path="/home/pi/Pictures"
+i="ls $path/precessed | wc -l"
+max="ls $path | wc -l"
+echo $path
+echo $i
+echo $max
+
 for f in $(ls | grep -i '[j-z]......................jpg');
 do	
 	i=$((i+1))
-	echo $(($i*100/21605))
-	ffmpeg -i "$f" -q:v 10 -hide_banner -loglevel error -vf scale=100:100 -y "processed/$f"
+	echo $(($i*100/$max))
+	ffmpeg -i "$f" -q:v 10 -hide_banner -loglevel error -vf scale=100:100 -y "$path/processed/$f"
 	
 done
 
