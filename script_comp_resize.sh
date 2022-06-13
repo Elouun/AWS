3e39a70444506adbbdc072e77e94618784e23d37
DeAntoine <Delacoux.antoine@gmail.com>
2022-06-13 11:34:28 +0100

    update script

--- script_comp_resize.sh
+++ script_comp_resize.sh
@@ -3,17 +3,18 @@
 path="/home/pi/Pictures"
 mkdir "$path/processed"
 
-i="ls $path/processed | wc -l"
-max="ls $path | wc -l"
+i=$(ls $path/processed | wc -l)
+max=$(ls $path | wc -l)
 echo $path
 echo $i
 echo $max
 
-for f in $(ls | grep -i '[j-z]......................jpg');
+for f in $(ls $path | grep -i '[0-z]......................jpg');
 do	
 	i=$((i+1))
 	echo $(($i*100/$max))
-	ffmpeg -i "$f" -q:v 10 -hide_banner -loglevel error -vf scale=100:100 -y "$path/processed/$f"
+	echo $f
+	#ffmpeg -i "$f" -q:v 10 -hide_banner -loglevel error -vf scale=100:100 -n "$path/processed/$f"
 	
 done
 
